6f4c8fd32f909d6d07555895cfcaf26851eef661
Duveimar <72568140+Duveimar@users.noreply.github.com>
2023-05-13 19:14:02 +0000

    se acomoda a mi idea

---
+++ script.js
@@ -0,0 +1,5 @@
+function iniciarJuego() {
+      alert("¡Bienvenido al juego! Presiona OK para comenzar.");
+        // Aquí puedes agregar el código para el juego o actividad que quieras ofrecer
+        }
+
